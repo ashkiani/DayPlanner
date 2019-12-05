e406db8840ef4ce3400c456ca7a63e2c1194e975
Sia <ashkiani@yahoo.com>
2019-12-04 20:49:54 -0500

    added event handler for the Save buttons

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function () {
 
   function updateTimeText() {
@@ -11,6 +12,7 @@ $(document).ready(function () {
     for (var i = 0; i < 9; i++) {
       rowEl = $("<div>");
       rowEl.addClass("row");
+      rowEl.attr("id", "row" + i);
       //time col
       colEl = $("<div>");
       colEl.addClass("col-md-1");
@@ -23,7 +25,8 @@ $(document).ready(function () {
       colEl.appendTo(rowEl);
       controlEl = $("<textarea>");
       controlEl.addClass("form-control");
-      controlEl.text("text");
+      controlEl.attr("id", "txt" + i);
+      //controlEl.text("text");
       controlEl.appendTo(colEl);
       //button col
       colEl = $("<div>");
@@ -31,15 +34,33 @@ $(document).ready(function () {
       colEl.appendTo(rowEl);
       controlEl = $("<button>");
       controlEl.addClass("btn btn-primary");
+      controlEl.attr("id", "btn" + i);
       controlEl.text("Save");
       controlEl.appendTo(colEl);
     }
   }
   AddControls();
+  function loadNotes() {
+
+  }
+  loadNotes();
 
   function updateTimeTextByInterval() {
     updateTimeText();
   }
 
   setInterval(updateTimeTextByInterval, 1000);
+
+
+  $(".btn").click(function () {
+    var txtID = $(this).attr('id');
+    txtID = txtID.replace("btn", "txt");
+    var txtAreaEl = $("#" + txtID);
+
+    alert(txtAreaEl.val());
+
+  });
+
+
+
 });
