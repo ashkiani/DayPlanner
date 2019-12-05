89e9ef6b5a7826c551c27de83ea5ab8d77a2e65a
Sia <ashkiani@yahoo.com>
2019-12-04 19:56:25 -0500

    Added code to dynamically add the controls/rows on main page

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,4 +1,45 @@
-$(document).ready(function(){
-    $("header").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
-    
-  });
+$(document).ready(function () {
+
+  function updateTimeText() {
+    $(".currentDate").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+  }
+  updateTimeText();
+  function AddControls() {
+    var rowEl;
+    var colEl;
+    var controlEl;
+    for (var i = 0; i < 9; i++) {
+      rowEl = $("<div>");
+      rowEl.addClass("row");
+      //time col
+      colEl = $("<div>");
+      colEl.addClass("col-md-1");
+      colEl.text((9 + i) + ":00");
+      colEl.appendTo(rowEl);
+      rowEl.appendTo($(".container"));
+      //text col
+      colEl = $("<div>");
+      colEl.addClass("col-md-10");
+      colEl.appendTo(rowEl);
+      controlEl = $("<textarea>");
+      controlEl.addClass("form-control");
+      controlEl.text("text");
+      controlEl.appendTo(colEl);
+      //button col
+      colEl = $("<div>");
+      colEl.addClass("col-md-1");
+      colEl.appendTo(rowEl);
+      controlEl = $("<button>");
+      controlEl.addClass("btn btn-primary");
+      controlEl.text("Save");
+      controlEl.appendTo(colEl);
+    }
+  }
+  AddControls();
+
+  function updateTimeTextByInterval() {
+    updateTimeText();
+  }
+
+  setInterval(updateTimeTextByInterval, 1000);
+});
